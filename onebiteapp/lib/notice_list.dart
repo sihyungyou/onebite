f0ca50004abe01b815e3c55be1d0f1ce47fff513
sihyungyou <sihyungyou@gmail.com>
2018-11-05 20:56:14 +0900

    notice_list dummy db success

--- onebiteapp/lib/notice_list.dart
+++ onebiteapp/lib/notice_list.dart
@@ -1,38 +1,85 @@
 // 공지사항 리스트업
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class NoticeListPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    final title = 'Basic List';
+class NoticeListPage extends StatefulWidget {
+ @override
+ _NoticeListPageState createState() {
+   return _NoticeListPageState();
+ }
+}
 
-    return MaterialApp(
-      title: title,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(title),
-        ),
-        body: ListView(
-          children: <Widget>[
-            ListTile(
-              // leading: Icon(Icons.map),
-              title: Text('[공지] 한입만 업데이트 안내'),
-              subtitle: Text('2018.11.9'),
-            ),
-            ListTile(
-              // leading: Icon(Icons.photo_album),
-              title: Text('[공지] 한입만 업데이트 안내'),
-              subtitle: Text('2018.10.13'),
-            ),
-            ListTile(
-              // leading: Icon(Icons.phone),
-              title: Text('[공지] 한입만 업데이트 안내'),
-              subtitle: Text('2018.9.8'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+class _NoticeListPageState extends State<NoticeListPage> {
+ @override
+ Widget build(BuildContext context) {
+   return Scaffold(
+     appBar: AppBar(title: Text('건의 사항')),
+     body: _buildBody(context),
+   );
+ }
+
+  // 지금 name, votes 두 필드로 이루어져있는데 여기서 
+  // name 이 공지사항제목
+  // votes -> date 으로 바꿔준 후, subtitle로 공지사항 업데이트 날짜
+ Widget _buildBody(BuildContext context) {
+   return StreamBuilder<QuerySnapshot>(
+     stream: Firestore.instance.collection('dummys').snapshots(),
+     builder: (context, snapshot) {
+       if (!snapshot.hasData) return LinearProgressIndicator();
+
+       return _buildList(context, snapshot.data.documents);
+     },
+   );
+ }
+
+ Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
+   return ListView(
+     padding: const EdgeInsets.only(top: 20.0),
+     children: snapshot.map((data) => _buildListItem(context, data)).toList(),
+   );
+ }
+
+ Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
+   final record = Record.fromSnapshot(data);
+
+   return Padding(
+     key: ValueKey(record.name),
+     padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+     child: Container(
+       decoration: BoxDecoration(
+         border: Border.all(color: Colors.grey),
+         borderRadius: BorderRadius.circular(5.0),
+       ),
+       child: ListTile(
+         title: Text(record.name),
+         trailing: Text(record.votes.toString()),
+         onTap: () => Firestore.instance.runTransaction((transaction) async {
+               final freshSnapshot = await transaction.get(record.reference);
+               final fresh = Record.fromSnapshot(freshSnapshot);
+
+               await transaction
+                   .update(record.reference, {'votes': fresh.votes + 1});
+             }),
+       ),
+     ),
+   );
+ }
+}
+
+class Record {
+ final String name;
+ final int votes;
+ final DocumentReference reference;
+
+ Record.fromMap(Map<String, dynamic> map, {this.reference})
+     : assert(map['name'] != null),
+       assert(map['votes'] != null),
+       name = map['name'],
+       votes = map['votes'];
+
+ Record.fromSnapshot(DocumentSnapshot snapshot)
+     : this.fromMap(snapshot.data, reference: snapshot.reference);
+
+ @override
+ String toString() => "Record<$name:$votes>";
 }
