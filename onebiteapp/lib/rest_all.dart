d4e2b80bed1668872fb51d6321ce751fa0007a01
gur5381 <21500670@handong.edu>
2018-11-25 06:02:47 +0900

    data passing succeeded, data organized

--- onebiteapp/lib/rest_all.dart
+++ onebiteapp/lib/rest_all.dart
@@ -16,113 +16,175 @@ class _RestAllPageState extends State<RestAllPage> {
   final FirebaseUser user;
   _RestAllPageState({Key key, this.user});
 
+  List<Restaurant> korean = new List<Restaurant>();
+  List<Restaurant> chinese = new List<Restaurant>();
+  List<Restaurant> japanese = new List<Restaurant>();
+  List<Restaurant> boonSick = new List<Restaurant>();
+  List<Restaurant> fastFood = new List<Restaurant>();
+
+
+
+  void _buildList() async {
+    print("buildlist in");
+    QuerySnapshot querySnapshot = await Firestore.instance.collection(
+        "restaurants").getDocuments();
+    var list = querySnapshot.documents;
+    print(list.length);
+    for (var i = 0; i < list.length; i++) {
+      final Restaurant restaurant = Restaurant.fromSnapshot(list[i]);
+      print(restaurant.name);
+      if (restaurant.type == 'korean')
+        korean.add(restaurant);
+      else if (restaurant.type == 'chinese')
+        chinese.add(restaurant);
+      else if (restaurant.type == 'japanese')
+        japanese.add(restaurant);
+      else if (restaurant.type == 'bookSick')
+        boonSick.add(restaurant);
+      else if (restaurant.type == 'fastFood') fastFood.add(restaurant);
+    }
+  }
+
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    print("init state yes");
+    _buildList();
+    print("init state over");
+
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home : DefaultTabController(
-        length: 5,
-        child: Scaffold(
-          appBar: AppBar(
-            bottom: TabBar(
-              tabs: <Widget>[
-                Tab(text: '패스트푸드'),
-                Tab(text: '한식'),
-                Tab(text: '중식'),
-                Tab(text: '일식'),
-                Tab(text: '분식'),
+        home : DefaultTabController(
+          length: 5,
+          child: Scaffold(
+            appBar: AppBar(
+              bottom: TabBar(
+                tabs: <Widget>[
+                  Tab(text: '패스트푸드'),
+                  Tab(text: '한식'),
+                  Tab(text: '중식'),
+                  Tab(text: '일식'),
+                  Tab(text: '분식'),
+                ],
+              ),
+              title: Text('전체 식당'),
+            ),
+            body: TabBarView(
+              children: <Widget>[
+
+                ListView.builder(
+                    itemCount: fastFood.length,
+                    itemBuilder: (context, index){
+                      return ListTile(
+                        title: Text(fastFood[index].name),
+                        subtitle: Text("영업시간: " + fastFood[index].time),
+
+                      );
+                    }
+
+                ),
+                ListView.builder(
+                    itemCount: korean.length,
+                    itemBuilder: (context, index){
+                      return ListTile(
+                        title: Text(korean[index].name),
+                        subtitle: Text("영업시간: " + korean[index].time),
+                        onTap: (){
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (BuildContext context) => DetailPage(user:user, restaurant: korean[index])))
+                              .catchError((e) => print(e));
+                        }
+
+                      );
+
+                    }
+
+                ),
+                ListView.builder(
+                    itemCount: chinese.length,
+                    itemBuilder: (context, index){
+                      return ListTile(
+                        title: Text(chinese[index].name),
+                        subtitle: Text("영업시간: " + chinese[index].time),
+
+                      );
+                    }
+
+                ),
+                ListView.builder(
+                    itemCount: japanese.length,
+                    itemBuilder: (context, index){
+                      return ListTile(
+                        title: Text(japanese[index].name),
+                        subtitle: Text("영업시간: " + japanese[index].time),
+
+                      );
+                    }
+                ),
+                ListView.builder(
+                    itemCount: boonSick.length,
+                    itemBuilder: (context, index){
+                      return ListTile(
+                        title: Text(boonSick[index].name),
+                        subtitle: Text("영업시간: " + boonSick[index].time),
+
+                      );
+                    }
+
+                ),
+
               ],
             ),
-            title: Text('전체 식당'),
           ),
-          body: TabBarView(
-            children: <Widget>[
-              FastList(),
-              KoreanList(),
-              Icon(Icons.directions_transit),
-              Icon(Icons.directions_bike),
-              Icon(Icons.directions_boat),
-            ],
-          ),
-        ),
-      )
+        )
     );
   }
 }
 
-class KoreanList extends StatelessWidget {
-  final FirebaseUser user;
-  KoreanList({Key key, this.user});
-  @override
-  Widget build(BuildContext context){
-    return new StreamBuilder(
-      stream: Firestore.instance.collection('restaurants').snapshots(),
-      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-        if(!snapshot.hasData) return new Text('Loading...');
-        return new ListView(
-          children: snapshot.data.documents.map((document) {
-            if (document['type'] == 'korean') {
-              return new ListTile(
-              // 사진
-              // 업체명
-              // 영업시간
-              // 배달비 여부
-              // favorite icon
-              title: new Text(document['name']),
-              subtitle: new Text(document['time']),
-              onTap: () {
-                // Navigator.push(
-                //   context,
-                //   MaterialPageRoute(
-                //     builder: (context) => DetailPage(user : user),
-                //   )
-                // );
-              },
-            );
-            } else {
-              return ListTile(
-                title: new Text('not korean'),
-                subtitle: new Text('how to get this out of listview..'),
-              );
-            }
-          }).toList()
-        );
-      },
-    );
-  }
-}
 
-class FastList extends StatelessWidget {
-  @override
-  Widget build(BuildContext context){
-    return new StreamBuilder(
-      stream: Firestore.instance.collection('restaurants').snapshots(),
-      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-        if(!snapshot.hasData) return new Text('Loading...');
-        return new ListView(
-          children: snapshot.data.documents.map((document) {
-            if (document['type'] == 'fastfood') {
-              return new ListTile(
-              // 사진
-              // 업체명
-              // 영업시간
-              // 배달비 여부
-              // favorite icon
-              title: new Text(document['name']),
-              subtitle: new Text(document['time']),
-              onTap: () {
-                
-              },
-            );
-            } else {
-              return ListTile(
-                title: new Text('not fastfood'),
-                subtitle: new Text('how to get this out of listview..'),
-              );
-            }
-          }).toList()
-        );
-      },
-    );
-  }
+
+
+
+class Restaurant {
+  final String closed;
+  final String deliveryFee;
+  final String minimumOrder;
+  final String name;
+  final DocumentReference reference;
+  final String phone;
+  final String rate;
+  final String time;
+  final String type;
+  Restaurant(this.closed, this.deliveryFee, this.minimumOrder, this.name, this.reference, this.phone, this.rate, this.time, this.type);
+
+  Restaurant.fromMap(Map<String, dynamic> map, {this.reference})
+      : assert(map['closed'] != null),
+        assert(map['delivery fee'] != null),
+        assert(map['minimum order'] != null),
+        assert(map['name'] != null),
+        assert(map['phone'] != null),
+        assert(map['rate'] != null),
+        assert(map['time'] != null),
+        assert(map['type'] != null),
+        closed = map['closed'],
+        deliveryFee = map['delivery fee'],
+        minimumOrder = map['minimum order'],
+        name = map['name'],
+        phone = map['phone'],
+        rate = map['rate'],
+        time = map['time'],
+        type = map['type'];
+
+
+  Restaurant.fromSnapshot(DocumentSnapshot snapshot)
+      : this.fromMap(snapshot.data, reference: snapshot.reference);
+
+
 }
--- onebiteapp/lib/rest_detail.dart
+++ onebiteapp/lib/rest_detail.dart
@@ -1,23 +1,63 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:Shrine/rest_all.dart';
 
 class DetailPage extends StatefulWidget {
-  // final FirebaseUser user;
-  
-  // DetailPage({Key key, this.user});
-  DetailPageState createState() => DetailPageState();
+  final FirebaseUser user;
+  final Restaurant restaurant;
+  List<Menu> menu = List<Menu>();
+  DetailPage({Key key, this.user, this.restaurant});
+  DetailPageState createState() => DetailPageState(user: this.user, restaurant: this.restaurant);
 }
 
 class DetailPageState extends State<DetailPage> {
-  // final FirebaseUser user;
-  // DetailPageState({Key key, this.user});
+   final FirebaseUser user;
+   final Restaurant restaurant;
+   DetailPageState({Key key, this.user, this.restaurant});
 
+   void _buildList() async {
+     QuerySnapshot querySnapshot = await Firestore.instance.collection("restaurants").document(restaurant.reference.documentID).collection('menu').getDocuments();
+     var list = querySnapshot.documents;
+     print(list.length);
+     for (var i = 0; i < list.length; i++) {
+       final Menu menu = Menu.fromSnapshot(list[i]);
+       print(menu.name);
+       print(menu.price);
+
+     }
+   }
+
+   @override
+  void initState() {
+    // TODO: implement initState
+    _buildList();
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // title: Text("${user.displayName}"),
-        title: Text('test'),
-      )
+
     );
   }
+}
+
+class Menu {
+  final String name;
+  final String price;
+  final DocumentReference reference;
+
+  Menu(this.name, this.price, this.reference);
+
+  Menu.fromMap(Map<String, dynamic> map, {this.reference})
+      : assert(map['name'] != null),
+        assert(map['price'] != null),
+        name = map['name'],
+        price = map['price'];
+
+
+  Menu.fromSnapshot(DocumentSnapshot snapshot)
+      : this.fromMap(snapshot.data, reference: snapshot.reference);
+
+
 }
