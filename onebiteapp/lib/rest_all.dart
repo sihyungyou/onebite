9c6e3a5fc525d5de29593cdd199e2bf115dd8fec
sihyungyou <sihyungyou@gmail.com>
2018-11-27 02:48:40 +0900

    rest_all completed

--- onebiteapp/lib/rest_all.dart
+++ onebiteapp/lib/rest_all.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'rest_detail.dart';
 
-
 class RestAllPage extends StatefulWidget {
   @override
   final FirebaseUser user;
@@ -12,7 +11,8 @@ class RestAllPage extends StatefulWidget {
   _RestAllPageState createState() => _RestAllPageState(user: user);
 }
 
-class _RestAllPageState extends State<RestAllPage> {
+class _RestAllPageState extends State<RestAllPage>
+    with SingleTickerProviderStateMixin {
   final FirebaseUser user;
   _RestAllPageState({Key key, this.user});
 
@@ -22,12 +22,12 @@ class _RestAllPageState extends State<RestAllPage> {
   List<Restaurant> boonSick = new List<Restaurant>();
   List<Restaurant> fastFood = new List<Restaurant>();
 
-
+  TabController _controller;
 
   Future _buildList() async {
     print("buildlist in");
-    QuerySnapshot querySnapshot = await Firestore.instance.collection(
-        "restaurants").getDocuments();
+    QuerySnapshot querySnapshot =
+        await Firestore.instance.collection("restaurant").getDocuments();
     var list = querySnapshot.documents;
     print(list.length);
     for (var i = 0; i < list.length; i++) {
@@ -42,13 +42,10 @@ class _RestAllPageState extends State<RestAllPage> {
           japanese.add(restaurant);
         else if (restaurant.type == 'bookSick')
           boonSick.add(restaurant);
-        else if (restaurant.type == 'fastFood') fastFood.add(restaurant);
+        else if (restaurant.type == 'fastfood') fastFood.add(restaurant);
       });
-
     }
   }
-
-
 
   @override
   void initState() {
@@ -56,46 +53,89 @@ class _RestAllPageState extends State<RestAllPage> {
     super.initState();
     print("init state yes");
     _buildList();
-
+    _controller = new TabController(length: 5, vsync: this);
     print("init state over");
-
   }
-
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        theme: ThemeData(fontFamily: 'NotoSans', primaryColor: Color.fromRGBO(255, 112, 74, 1)),
-        home : DefaultTabController(
-          length: 5,
-          child: Scaffold(
-            appBar: AppBar(
-              bottom: TabBar(
-                tabs: <Widget>[
-                  Tab(text: '패스트푸드'),
-                  Tab(text: '한식'),
-                  Tab(text: '중식'),
-                  Tab(text: '일식'),
-                  Tab(text: '분식'),
-                ],
+    final ThemeData theme = Theme.of(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('전체 식당'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {
+              Navigator.pushNamed(context, '/add');
+            },
+          )
+        ],
+      ),
+      body: ListView(
+        children: <Widget>[
+          new Container(
+            child: new TabBar(
+              indicator: UnderlineTabIndicator(
+                borderSide: BorderSide(width: 2.0, color: theme.primaryColor),
               ),
-              title: Text('전체 식당'),
+              controller: _controller,
+              tabs: <Widget>[
+                new Tab(
+                  child: Text('패스트푸드',
+                      style: TextStyle(
+                        color: Colors.black,
+                      )),
+                ),
+                new Tab(
+                  child: Text('한식',
+                      style: TextStyle(
+                        color: Colors.black,
+                      )),
+                ),
+                new Tab(
+                  child: Text('중식',
+                      style: TextStyle(
+                        color: Colors.black,
+                      )),
+                ),
+                new Tab(
+                  child: Text('일식',
+                      style: TextStyle(
+                        color: Colors.black,
+                      )),
+                ),
+                new Tab(
+                  child: Text('분식',
+                      style: TextStyle(
+                        color: Colors.black,
+                      )),
+                ),
+              ],
             ),
-            body: TabBarView(
+          ),
+          new Container(
+            height: 300.0,
+            child: new TabBarView(
+              controller: _controller,
               children: <Widget>[
-
+                // fastfood
                 ListView.builder(
                     itemCount: fastFood.length,
                     itemBuilder: (context, index){
                       return ListTile(
                         title: Text(fastFood[index].name),
                         subtitle: Text("영업시간: " + fastFood[index].time),
-
+                        onTap: (){
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (BuildContext context) => DetailPage(user:user, restaurant: fastFood[index])))
+                              .catchError((e) => print(e));
+                        }
                       );
                     }
-
                 ),
-                ListView.builder(
+              // korean
+              ListView.builder(
                     itemCount: korean.length,
                     itemBuilder: (context, index){
                       return ListTile(
@@ -106,56 +146,160 @@ class _RestAllPageState extends State<RestAllPage> {
                               builder: (BuildContext context) => DetailPage(user:user, restaurant: korean[index])))
                               .catchError((e) => print(e));
                         }
-
                       );
-
                     }
-
                 ),
+                // chinese
                 ListView.builder(
                     itemCount: chinese.length,
                     itemBuilder: (context, index){
                       return ListTile(
                         title: Text(chinese[index].name),
                         subtitle: Text("영업시간: " + chinese[index].time),
-
+                        onTap: (){
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (BuildContext context) => DetailPage(user:user, restaurant: chinese[index])))
+                              .catchError((e) => print(e));
+                        }
                       );
                     }
-
                 ),
+                // japanese
                 ListView.builder(
                     itemCount: japanese.length,
                     itemBuilder: (context, index){
                       return ListTile(
                         title: Text(japanese[index].name),
                         subtitle: Text("영업시간: " + japanese[index].time),
-
+                        onTap: (){
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (BuildContext context) => DetailPage(user:user, restaurant: japanese[index])))
+                              .catchError((e) => print(e));
+                        }
                       );
                     }
                 ),
+                // boonsick
                 ListView.builder(
                     itemCount: boonSick.length,
                     itemBuilder: (context, index){
                       return ListTile(
                         title: Text(boonSick[index].name),
                         subtitle: Text("영업시간: " + boonSick[index].time),
-
+                        onTap: (){
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (BuildContext context) => DetailPage(user:user, restaurant: boonSick[index])))
+                              .catchError((e) => print(e));
+                        }
                       );
                     }
-
                 ),
-
               ],
             ),
-          ),
-        )
+          )
+        ],
+      ),
     );
   }
+  // @override
+  // Widget build(BuildContext context) {
+  //   return MaterialApp(
+  //       theme: ThemeData(fontFamily: 'NotoSans', primaryColor: Color.fromRGBO(255, 112, 74, 1)),
+  //       home : DefaultTabController(
+  //         length: 5,
+  //         child: Scaffold(
+  //           appBar: AppBar(
+  //             bottom: TabBar(
+  //               tabs: <Widget>[
+  //                 Tab(text: '패스트푸드'),
+  //                 Tab(text: '한식'),
+  //                 Tab(text: '중식'),
+  //                 Tab(text: '일식'),
+  //                 Tab(text: '분식'),
+  //               ],
+  //             ),
+
+  //             title: Text('전체 식당'),
+  //             actions: <Widget>[
+  //               IconButton(
+  //                 icon: Icon(Icons.add),
+  //                 onPressed: () {
+  //                   Navigator.pushNamed(context, '/add');
+  //                 },
+  //               )
+  //             ],
+  //           ),
+  //           body: TabBarView(
+  //             children: <Widget>[
+
+  //               ListView.builder(
+  //                   itemCount: fastFood.length,
+  //                   itemBuilder: (context, index){
+  //                     return ListTile(
+  //                       title: Text(fastFood[index].name),
+  //                       subtitle: Text("영업시간: " + fastFood[index].time),
+
+  //                     );
+  //                   }
+
+  //               ),
+  //               ListView.builder(
+  //                   itemCount: korean.length,
+  //                   itemBuilder: (context, index){
+  //                     return ListTile(
+  //                       title: Text(korean[index].name),
+  //                       subtitle: Text("영업시간: " + korean[index].time),
+  //                       onTap: (){
+  //                         Navigator.of(context).push(MaterialPageRoute(
+  //                             builder: (BuildContext context) => DetailPage(user:user, restaurant: korean[index])))
+  //                             .catchError((e) => print(e));
+  //                       }
+
+  //                     );
+
+  //                   }
+
+  //               ),
+  //               ListView.builder(
+  //                   itemCount: chinese.length,
+  //                   itemBuilder: (context, index){
+  //                     return ListTile(
+  //                       title: Text(chinese[index].name),
+  //                       subtitle: Text("영업시간: " + chinese[index].time),
+
+  //                     );
+  //                   }
+
+  //               ),
+  //               ListView.builder(
+  //                   itemCount: japanese.length,
+  //                   itemBuilder: (context, index){
+  //                     return ListTile(
+  //                       title: Text(japanese[index].name),
+  //                       subtitle: Text("영업시간: " + japanese[index].time),
+
+  //                     );
+  //                   }
+  //               ),
+  //               ListView.builder(
+  //                   itemCount: boonSick.length,
+  //                   itemBuilder: (context, index){
+  //                     return ListTile(
+  //                       title: Text(boonSick[index].name),
+  //                       subtitle: Text("영업시간: " + boonSick[index].time),
+
+  //                     );
+  //                   }
+
+  //               ),
+
+  //             ],
+  //           ),
+  //         ),
+  //       )
+  //   );
+  // }
 }
-
-
-
-
 
 class Restaurant {
   final String closed;
@@ -167,7 +311,8 @@ class Restaurant {
   final String rate;
   final String time;
   final String type;
-  Restaurant(this.closed, this.deliveryFee, this.minimumOrder, this.name, this.reference, this.phone, this.rate, this.time, this.type);
+  Restaurant(this.closed, this.deliveryFee, this.minimumOrder, this.name,
+      this.reference, this.phone, this.rate, this.time, this.type);
 
   Restaurant.fromMap(Map<String, dynamic> map, {this.reference})
       : assert(map['closed'] != null),
@@ -187,9 +332,6 @@ class Restaurant {
         time = map['time'],
         type = map['type'];
 
-
   Restaurant.fromSnapshot(DocumentSnapshot snapshot)
       : this.fromMap(snapshot.data, reference: snapshot.reference);
-
-
 }
