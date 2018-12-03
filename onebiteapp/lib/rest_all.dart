7586be09a806f284039dea2cfb28015924eda915
sihyungyou <sihyungyou@gmail.com>
2018-12-03 10:22:19 +0900

    tabbar cor completed

--- onebiteapp/lib/rest_all.dart
+++ onebiteapp/lib/rest_all.dart
@@ -92,40 +92,56 @@ class _RestAllPageState extends State<RestAllPage>
         children: <Widget>[
           new Container(
             child: new TabBar(
+              isScrollable: true,
               indicator: UnderlineTabIndicator(
                 borderSide: BorderSide(width: 2.0, color: theme.primaryColor),
               ),
               controller: _controller,
               tabs: <Widget>[
-                new Tab(
-                  child: Text('패스트푸드',
-                      style: TextStyle(
-                        color: Colors.black,
-                      )),
+                SizedBox(
+                  width: 70.0,
+                  child: new Tab(
+                    child: Text('패스트푸드',
+                        style: TextStyle(
+                          color: Colors.black,
+                        )),
+                  ),
                 ),
-                new Tab(
-                  child: Text('한식',
-                      style: TextStyle(
-                        color: Colors.black,
-                      )),
+                SizedBox(
+                  width: 41.0,
+                    child: new Tab(
+                    child: Text('한식',
+                        style: TextStyle(
+                          color: Colors.black,
+                        )),
+                  ),
                 ),
-                new Tab(
-                  child: Text('중식',
-                      style: TextStyle(
-                        color: Colors.black,
-                      )),
+                SizedBox(
+                  width: 41.0,
+                    child: new Tab(
+                    child: Text('중식',
+                        style: TextStyle(
+                          color: Colors.black,
+                        )),
+                  ),
                 ),
-                new Tab(
-                  child: Text('일식',
-                      style: TextStyle(
-                        color: Colors.black,
-                      )),
+                SizedBox(
+                  width: 41.0,
+                    child: new Tab(
+                    child: Text('일식',
+                        style: TextStyle(
+                          color: Colors.black,
+                        )),
+                  ),
                 ),
-                new Tab(
-                  child: Text('분식',
-                      style: TextStyle(
-                        color: Colors.black,
-                      )),
+                SizedBox(
+                  width: 41.0,
+                    child: new Tab(
+                    child: Text('분식',
+                        style: TextStyle(
+                          color: Colors.black,
+                        )),
+                  ),
                 ),
               ],
             ),
